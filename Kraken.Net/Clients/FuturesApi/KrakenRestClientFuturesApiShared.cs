afa1c3e237a39864205ae2c36e101a9f84176cc5
Jkorf <jankorf91@gmail.com>
2026-08-13 12:42:44 +0200

    Added Shared APIs Rest integration tests

--- Kraken.Net/Clients/FuturesApi/KrakenRestClientFuturesApiShared.cs
+++ Kraken.Net/Clients/FuturesApi/KrakenRestClientFuturesApiShared.cs
@@ -436,7 +436,7 @@ namespace Kraken.Net.Clients.FuturesApi
 
         #region Open Interest client
 
-        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, true);
+        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, false);
         async Task<HttpResult<SharedOpenInterest>> IOpenInterestRestClient.GetOpenInterestAsync(GetOpenInterestRequest request, CancellationToken ct)
         {
             var validationError = SharedClient.GetOpenInterestOptions.ValidateRequest(request, this);
